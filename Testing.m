3b3670b5dac4f6b9a415a342a19378443502d3e7
muhamedhassan1 <75187339+muhamedhassan1@users.noreply.github.com>
2022-01-08 00:48:55 +0200

    Add files via upload

--- Testing.m
+++ Testing.m
@@ -6,7 +6,7 @@ face = imcrop(Img,Faces(1,:));
 face = imresize(face,[320 243]);
 enhanced = MSRCR_go(face,[15, 80, 250],[1/3,1/3,1/3],125,46,0.354,352);
 Features=Get_wavelet_features(enhanced,2);
-simiarityy  = sum(FM(:,i).*Features)/sqrt(sum(FM(:,i).^2)*sum(Features.^2));;
+simiarityy  = sum(FM(:,i).*Features(:))/sqrt(sum(FM(:,i).^2)*sum(Features(:).^2));;
 [minn,index] = min(simiarityy);
 if minn > 0.6
     result = 'true';
